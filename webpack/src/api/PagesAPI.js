20e0c45b53470aebd03bc487c56a99f804cff94f
natekandler <natekandler@gmail.com>
2016-03-17 07:24:10 -0700

    updates pages api

--- webpack/src/api/PagesAPI.js
+++ webpack/src/api/PagesAPI.js
@@ -3,7 +3,7 @@ const baseURL = __DEVELOPMENT__ ? "http://127.0.0.1:3000" : "";
 
 
 export function getPages(){
-  webAPI('/pages.json').then(function (response){
+  return fetch('http://localhost:3000/pages.json').then(function (response){
     response.json().then(function(data){
       console.warn(data)
       return data
