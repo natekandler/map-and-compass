7fc72a7cb2d99bdc272a4caab036aaab9d538666
natekandler <natekandler@gmail.com>
2016-03-17 20:30:20 -0700

    continues work on fetch

--- webpack/src/api/PagesAPI.js
+++ webpack/src/api/PagesAPI.js
@@ -1,9 +1,30 @@
 export function getPages(){
-  return fetch('http://localhost:3000/pages.json').then(function (response){
-    response.json().then(function(data){
-      console.warn(data)
-      return data
+  return () => {
+    fetch('http://localhost:3000/pages.json').then(function (response){
+      return response.json()
     });
-  });
+  }
 }
 
+
+export function getPages2(){
+  return () => {
+    fetch(('http://localhost:3000/pages.json'), {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      if (json.session_error) {
+        console.warn(json.session_error)
+      } else {
+        return json
+      }
+    }).catch((error) => {
+      console.warn("error") 
+    }) 
+  }
+}
--- webpack/src/views/Pages/PagesIndex.js
+++ webpack/src/views/Pages/PagesIndex.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
-import { getPages, parseJson } from '../../api/PagesAPI'
+import { getPages, getPages2 } from '../../api/PagesAPI'
+
+function renderPages(){
+  var pages = getPages2();
+  for(page in pages){
+    console.warn(page)
+  }
+}
 
 export default class PagesIndex extends Component {
 
   render() {
-    var pages = getPages();
+    var pages = getPages2();
     console.warn(pages)
     return (
       <div>
         <h1>PagesIndex</h1>
-        
+        {renderPages()}    
       </div>
     );
   }
