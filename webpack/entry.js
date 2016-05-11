756de4c294f961cfc2d25d16924dce862fad1b1f
Nate Kandler <natekandler@gmail.com>
2016-05-11 11:44:11 -0700

    fixes errors to get pages index to load, redux is still not setting state

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Router, Route, Link, browserHistory } from 'react-router'
 import Home from "./src/views/Home/Home"
 import PagesIndex from "./src/views/Pages/PagesIndex"
-import { createHistory } from 'history';
 import configureStore from './src/redux/store/configure_store';
-import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 require('./src/styles/style.scss');
 
-const store = configureStore();
-const history = createHistory();
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  })
+)
 
-syncReduxAndRouter(history, store);
+const history = syncHistoryWithStore(browserHistory, store)
 
 render((
   <Provider store={store}>
     <div>
-      <Router history={browserHistory}>
+      <Router history={history}>
         <Route path="/" component={Home}>
         </Route>
         <Route path="/Pages" component={PagesIndex}>
